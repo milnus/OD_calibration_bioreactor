9861d75f22095bef62e46049843d6a5914312706
teddygroves <groves.teddy@gmail.com>
2024-12-03 16:09:50 +0100

    Format with styler

--- R/origin_point_box.R
+++ R/origin_point_box.R
@@ -1,7 +1,8 @@
 #### Function to allow user to select addition of a point through (0,0) ####
-origin_point_box <- function(){
+origin_point_box <- function() {
   checkboxInput(
     inputId = "origin_point",
     label = "Add (0,0) point",
-    value = FALSE)
+    value = FALSE
+  )
 }
--- R/ui_num_od_read.R
+++ R/ui_num_od_read.R
@@ -1,15 +1,17 @@
-ui_num_od_read <- function(calibration_table){
+ui_num_od_read <- function(calibration_table) {
   print("TEST")
   print(calibration_table)
-  if(is.null(calibration_table)){
+  if (is.null(calibration_table)) {
     return()
-  } else{
-    if(all(is.na(calibration_table["pio_od"]))){
+  } else {
+    if (all(is.na(calibration_table["pio_od"]))) {
       print("in")
-      out_ui <- numericInput(inputId = "x_pio_ods", 
-                   label = "Number of first and last PioReactor OD readings to use for calibration", 
-                   value = 10, min = 1, max = 50)
-     return(out_ui)
+      out_ui <- numericInput(
+        inputId = "x_pio_ods",
+        label = "Number of first and last PioReactor OD readings to use for calibration",
+        value = 10, min = 1, max = 50
+      )
+      return(out_ui)
     }
   }
 }
